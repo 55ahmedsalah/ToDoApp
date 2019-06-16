5f691218980049c3bd7c789d758f80cbb1778f42
Ahmed M. Salah <ahmed.mohamed973@eng-st.cu.edu.eg>
2019-06-16 10:16:12 +0200

    app.js Modifications

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,22 @@ const signinRoutes = require('./routes/user')
 
 const app = express();
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
+
 app.use(express.static('static'));
 
 mongoose.connect('mongodb+srv://ahmed:msgpu2xHVBylyqV0@cluster0-stl5s.mongodb.net/to-do-app?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true })
